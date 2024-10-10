fe6430dbd9868f64a7ec03f97c908b0df0d7c5c1
Peter Harry <prharry@hotmail.com>
2024-10-11 06:14:11 +1000

    Add a Watch Companion the the App
    Add initial files for the Watch Companion

---
+++ OpenVehicleApp/companionWatch Watch App/companionWatchApp.swift
@@ -0,0 +1,18 @@
+//
+//  companionWatchApp.swift
+//  companionWatch Watch App
+//
+//  Created by Peter Harry on 11/10/2024.
+//  Copyright © 2024 Open Vehicle Systems. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct companionWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WatchView()
+        }
+    }
+}
